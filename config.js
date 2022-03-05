d53a7e090f82f04b207d8f35ceeed021b641ebeb
macet00100 <93109019+macet00100@users.noreply.github.com>
2022-03-05 15:17:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
+const nameGirl = 'Các bạn nữ ';
+const giftUrl = 'https://www.facebook.com/groups/388676249351013';
+const eventName = 'Chúc Mừng 08-03';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
-
+const contentCard = 'Chúc các bạn nữ của chúng mình 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với các bạn trong hôm nay và cả những ngày sau';
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
