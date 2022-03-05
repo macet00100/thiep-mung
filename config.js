06bfda24cd9c3f8768966356d7d155d6fcbd6234
macet00100 <93109019+macet00100@users.noreply.github.com>
2022-03-05 17:01:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const nameGirl = 'Các bạn nữ K24TCB ';
+const nameGirl = 'Các bạn nữ K24TCB';
 const giftUrl = 'https://www.facebook.com/groups/388676249351013';
 const eventName = 'Chúc Mừng 08-03';
 const titleCard = 'Tặng các công chúa xinh đẹp của K24 TCB';
