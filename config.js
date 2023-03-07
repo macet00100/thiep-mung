967cb55b15531a99d3e157bace4c71338be27ebf
macet00100 <93109019+macet00100@users.noreply.github.com>
2023-03-07 13:46:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
-const nameGirl = 'Các bạn nữ K24TCB';
-const giftUrl = 'https://www.facebook.com/messages/t/3900454440054459';
+const nameGirl = 'Các bông hồng của HYS Đống Đa';
+const giftUrl = 'https://www.facebook.com/groups/703536489670408';
 const eventName = 'Chúc Mừng 08-03';
-const titleCard = 'Tặng các công chúa xinh đẹp của K24 TCB';
+const titleCard = 'Tặng các công chúa xinh đẹp của HYS DODA';
 const contentCard = 'Chúc các bạn nữ của chúng mình 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với các bạn trong hôm nay và cả những ngày sau';
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = '83.jpg';
+const giftImage = 'hys.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
