7981a82da653405dd210ce25306e597db3404663
macet00100 <93109019+macet00100@users.noreply.github.com>
2023-03-08 04:28:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Chị em HYS Đống Đa';
-const giftUrl = 'https://www.facebook.com/groups/703536489670408';
+const giftUrl = 'https://www.facebook.com/groups/703536489670408/posts/6354022067955127/';
 const eventName = 'Chúc Mừng 08-03';
 const titleCard = 'Tặng các công chúa xinh đẹp của HYS DODA';
 const contentCard = 'Chúc các bạn nữ của chúng mình 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với các bạn trong hôm nay và cả những ngày sau';
